3a2080e84667dd07d9356aaa343062915bcb1a57
Andry Garnov <garrus322000@gmail.com>
2025-11-05 10:37:18 +0500

    Добавил и настроил router-outlet и роуты

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'tasks',
+    pathMatch: 'full'
+  },
+  {
+    path: 'tasks',
+    loadComponent: () => import('./components/to-do-list').then(c => c.ToDoList),
+    title: 'Tasks',
+    pathMatch: 'full'
+  },
+  {
+    path: '**',
+    redirectTo: 'tasks',
+    pathMatch: 'full'
+  }
+];
